fc64323bee3c1348f3d57363232156d7a43f5a17
pzoli <papp.zoltan@infokristaly.hu>
2024-09-02 19:50:52 +0200

    escape command string parameter

--- scan.ts
+++ scan.ts
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { exec } = require("child_process");
+const { exec } = require('child_process');
 
 const OUTPUT_FILE_NAME = 'scanned.jpg';
 
@@ -7,23 +7,33 @@ const OUTPUT_FILE_NAME = 'scanned.jpg';
 //const color_mode = 2; //grayscale : Gray
 //const color_mode = 4; //back-white : Lineart
 
-async function scanImage(assetId: string, color_mode: number, resolution: number) : Promise<string> {
-	const mode = color_mode == 1 ? "Color" : color_mode == 2 ? "Gray" : "Lineart"
+async function scanImage(
+	assetId: string,
+	color_mode: number,
+	resolution: number,
+): Promise<string> {
+	const mode =
+		color_mode == 1 ? 'Color' : color_mode == 2 ? 'Gray' : 'Lineart';
+	assetId = assetId.replace(/\\/g, '\\\\').replace(/\"/g, '\\"');
 	const promise = new Promise((resolve, reject) => {
-		const scanProc = exec(`scanimage -d "${assetId}" --format tiff --mode ${mode} --resolution ${resolution} -l 0mm -t 0mm|convert - -quality 75 scanned.jpg`, (error: any, stdout: any, stderr: any) => {
-			if (error) {
-				reject(`error: ${error.message}`)
-			}
-			resolve(OUTPUT_FILE_NAME)
-		});
-	})
-	var result: string
+		const command = `scanimage -d "${assetId}" --format tiff --mode ${mode} --resolution ${resolution} -l 0mm -t 0mm|convert - -quality 75 scanned.jpg`;
+		const scanProc = exec(
+			command,
+			(error: any, stdout: any, stderr: any) => {
+				if (error) {
+					reject(`error: ${error.message}`);
+				}
+				resolve(OUTPUT_FILE_NAME);
+			},
+		);
+	});
+	var result: string;
 	try {
-		result = await promise as string
- 	} catch (e) {
-		result = e as string
+		result = (await promise) as string;
+	} catch (e) {
+		result = e as string;
 	}
-	return result
+	return result;
 }
 
 async function getDeviceInfos(): Promise<string> {
@@ -36,29 +46,32 @@ async function getDeviceInfos(): Promise<string> {
 	};
 
 	const promise = new Promise((resolve, reject) => {
-		const ls = exec("scanimage -f \"%d\t%m\n\"", (error: any, stdout: any, stderr: any) => {
-			if (error) {
-				reject(`error: ${error.message}`)
-			}
-			if (stderr) {
-				reject(`stderr: ${stderr}`)
-			}
-			resolve(stdout)
-		});
-	})
+		const scanProc = exec(
+			'scanimage -f "%d\t%m\n"',
+			(error: any, stdout: any, stderr: any) => {
+				if (error) {
+					reject(`error: ${error.message}`);
+				}
+				if (stderr) {
+					reject(`stderr: ${stderr}`);
+				}
+				resolve(stdout);
+			},
+		);
+	});
 	try {
-		const devicesStr = await promise as string
-		const lines = devicesStr.split("\n")
-		lines.forEach(deviceInfoLine => {
-			if (deviceInfoLine != "") {
-				const  l = deviceInfoLine.split("\t")
+		const devicesStr = (await promise) as string;
+		const lines = devicesStr.split('\n');
+		lines.forEach((deviceInfoLine) => {
+			if (deviceInfoLine != '') {
+				const l = deviceInfoLine.split('\t');
 				if (l.length > 1) {
-					result.devices.push({assetId:l[0],assetName:l[1]})
+					result.devices.push({ assetId: l[0], assetName: l[1] });
 				}
 			}
-		})		
- 	} catch (e) {
-		console.log("exceptoin:" + e)
+		});
+	} catch (e) {
+		console.log('exceptoin:' + e);
 	}
 
 	//result.devices = [{ assetId: 1, assetName: 'autobot' }];
